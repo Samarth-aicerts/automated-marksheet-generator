e5c4dc1cf4f4ee5f041f9d2ca8677b05fbab58fe
Samarth Patel <samarth@aicerts.io>
2026-07-08 13:05:57 +0530

    Create user model

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Document } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  googleId: string;
+  accessToken: string;
+  refreshToken: string;   
+}
+
+const userSchema = new Schema<IUser>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  googleId: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: String,
+    required: true,
+  },
+},
+{
+  timestamps: true,
+}
+);
+
+const User = model<IUser>("User", userSchema);
+
+export default User;
